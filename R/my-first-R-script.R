6d834e02fc9dd8409203ff331aea58ea9563f617
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-10-10 11:30:48 +0200

    gitautosync update 11

--- R/my-first-R-script.R
+++ R/my-first-R-script.R
@@ -1,6 +1,9 @@
+#### LOADING PACKAGES ####
 library(tidyverse)
 # install.packages("here")
 library(here)
-here("R", "my-first-R-script.R")
-"R/my-first-R-script.R"
+# here("R", "my-first-R-script.R")
+# "R/my-first-R-script.R"
+
+####### LOADING DATA ####
 
